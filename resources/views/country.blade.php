f04e07ba8c7a5d813f349e5cbfd5ee2178931921
AKASH <mdrakash32@gmail.com>
2022-11-16 00:44:49 +0600

    project setup

---
+++ resources/views/country.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Country CRUD</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+</head>
+<body>
+    <h1>Welcome to CSS</h1>
+
+    <script src="{{ asset('js/index.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('datatables/js/dataTables.bootstrap.min.js') }}"></script>
+    <script src="{{ asset('datatables/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('sweetalaert2/sweetalert2.min.js') }}"></script>
+    <script src="{{ asset('toostr/toastr.min.js') }}"></script>
+</body>
+</html>
